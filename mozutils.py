85231a054d6cfb8bbe590a1998bea80569aed050
Jon Coppeard <jon@coppeard.net>
2013-08-14 11:51:18 +0100

    Make jsbuild use common utils in mozutils.py and work on output filtering

--- mozutils.py
+++ mozutils.py
@@ -1,5 +1,6 @@
 # Common functions for python build scripts
 
+import re
 import sys
 import subprocess
 
@@ -9,12 +10,23 @@ def which(name):
     except subprocess.CalledProcessError:
         return None
 
+timeStampRe = re.compile(" *[\d\.:]+ (.+)")
+
+def stripTimestamp(line):
+    match = timeStampRe.match(line)
+    if not match:
+        return line
+    return match.group(1)
+
+objectFileRe = re.compile("( *[\d\.:]+ )?\w+\.o")
+
 def run_command(command, verbose, warnings):
     if verbose:
         print(command)
     proc = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.STDOUT,
                             shell = True)
     directory_line = None
+    sawError = False
     while proc.poll() == None:
         line = proc.stdout.readline().rstrip()
         if not line:
@@ -24,10 +36,18 @@ def run_command(command, verbose, warnings):
             print(line)
         elif "Entering directory" in line:
             directory_line = line
-        elif "error:" in line or (warnings and "warning:" in line):
+        elif "error:" in line or sawError:
             if directory_line:
-                print(directory_line)
+                print(stripTimestamp(directory_line))
                 directory_line = None
+            print(stripTimestamp(line))
+            sawError = True
+        elif warnings and "warning:" in line:
+            if directory_line:
+                print(stripTimestamp(directory_line))
+                directory_line = None
+            print(stripTimestamp(line))
+        elif objectFileRe.match(line):
             print(line)
 
     if proc.returncode:
