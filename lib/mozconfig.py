a9ca699eafcbbf353aa55e9cc489c7c106690213
Jon Coppeard <jonco@pobox.com>
2021-01-13 10:31:14 +0000

    Add asan option

--- lib/mozconfig.py
+++ lib/mozconfig.py
@@ -15,7 +15,9 @@ def add_common_config_arguments(parser):
     parser.add_argument('--32bit', action='store_true', dest='target32',
                         help='Cross compile 32bit build on 64bit host')
 
-    parser.add_argument('--tsan', action='store_true', help='Thread sanitizer build')
+    san_group = parser.add_mutually_exclusive_group()
+    san_group.add_argument('--tsan', action='store_true', help='Thread sanitizer build')
+    san_group.add_argument('--asan', action='store_true', help='Address sanitizer build')
 
 def add_browser_config_arguments(parser):
     parser.add_argument('--minimal', action='store_true',
@@ -26,7 +28,6 @@ def get_configs_from_args(args):
     options = []
 
     options.append("--with-ccache=$HOME/.mozbuild/sccache/sccache")
-    options.append("--enable-warnings-as-errors")
 
     if args.opt:
         names.append('opt')
@@ -47,26 +48,18 @@ def get_configs_from_args(args):
         options.append('--target=i686-pc-linux')
 
     if args.tsan:
-        # From https://firefox-source-docs.mozilla.org/tools/sanitizer/tsan.html
         names.append('tsan')
         options.append('--enable-thread-sanitizer')
-        # export RUSTFLAGS="-Zsanitizer=thread"
-        options.append('--disable-jemalloc')
-        options.append('--disable-profiling')
-        # export MOZ_DEBUG_SYMBOLS=1
-        options.append('--enable-debug-symbols')
-        options.append('--disable-install-strip')
-        if '--enable-optimize' in options:
-            options.remove('--enable-optimize')
-            options.append('--enable-optimize="-O2 -gline-tables-only"')
-        if not args.shell:
-            options.append('--disable-elf-hack')
-            options.append('--disable-crashreporter')
-            options.append('--disable-sandbox')
+        add_sanitizer_options(args, options)
+    elif args.asan:
+        names.append('asan')
+        options.append('--enable-address-sanitizer')
+        add_sanitizer_options(args, options)
 
     if getattr(args, 'shell', False):
         names.append('shell')
         options.append('--enable-application=js')
+        options.append("--enable-warnings-as-errors")
 
     if getattr(args, 'minimal', False):
         names.append('minimal')
@@ -80,6 +73,24 @@ def get_configs_from_args(args):
         options.append('--disable-webrtc')
 
     return names, options
+
+def add_sanitizer_options(args, options):
+    # From https://firefox-source-docs.mozilla.org/tools/sanitizer/tsan.html
+    # See also build/unix/mozconfig.tsan, mozconfig.asan
+    options.append('--disable-jemalloc')
+    options.append('--disable-profiling')
+    options.append('--enable-debug-symbols')
+    options.append('--disable-install-strip')
+    if '--enable-optimize' in options:
+        options.remove('--enable-optimize')
+        options.append('--enable-optimize="-O2 -gline-tables-only"')
+    if not args.shell:
+        options.append('--disable-elf-hack')
+        options.append('--disable-crashreporter')
+        options.append('--disable-sandbox')
+    # todo
+    # export RUSTFLAGS="-Zsanitizer=thread"
+    # export MOZ_DEBUG_SYMBOLS=1
 
 def get_build_name(config_names):
     """
