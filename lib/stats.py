9e7e2bdcaf2d3616b8de5860e1ea34ccac41a72e
Jon Coppeard <jonco@pobox.com>
2021-07-26 14:28:07 +0100

    Factor out stats code

---
+++ lib/stats.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Caclulate some basic statistics on a list of samples.
+
+import math
+
+class Stats:
+    def __init__(self, results, compareTo = None):
+        self.count = len(results)
+        self.mean, self.stdv = Stats.meanstdv(results)
+        self.cofv = self.stdv / self.mean
+        self.min = min(results)
+        self.max = max(results)
+        self.samples = results
+
+        self.diff = None
+        if compareTo:
+            self.diff = (self.mean - compareTo) / compareTo
+
+    def meanstdv(x):
+        assert(isinstance(x, list))
+
+        # from http://www.physics.rutgers.edu/~masud/computing/WPark_recipes_in_python.html
+        n, mean, std = len(x), 0, 0
+        for a in x:
+            mean = mean + a
+        mean = mean / float(n)
+        for a in x:
+            std = std + (a - mean) ** 2
+        if n > 1:
+            std = math.sqrt(std / float(n - 1))
+        else:
+            std = 0.0
+        return mean, std
